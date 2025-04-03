146cbf617d870bfef5c84c50bbcc30d65e4ac071
Abirdeen <102402217+Abirdeen@users.noreply.github.com>
2025-04-03 20:24:15 +0100

    feat: implement BLOSOM algorithm

--- EditDistance.go
+++ EditDistance.go
@@ -70,3 +70,62 @@ func EditDistance(str1 string, str2 string, edit_map map[edit_key]edit_info) map
 		return edit_map
 	}
 }
+
+func EditDistanceBLOSUM(str1 string, str2 string, edit_map map[edit_key]edit_info, BLOSOM_matrix map[string]map[string]int) map[edit_key]edit_info {
+
+	m := len(str1)
+	n := len(str2)
+
+	if str1 == "" {
+		edit_map[edit_key{0, n}] = edit_info{strings.Repeat("I", n), (n * -8)}
+		return edit_map
+	}
+	if str2 == "" {
+		edit_map[edit_key{m, 0}] = edit_info{strings.Repeat("D", m), (m * -8)}
+		return edit_map
+	}
+
+	if edit_map[edit_key{m - 1, n}].transcript == "" {
+		delete_map := EditDistanceBLOSUM(str1[0:m-1], str2, edit_map, BLOSOM_matrix)
+		maps.Copy(edit_map, delete_map)
+	}
+	delete_transcript := edit_map[edit_key{m - 1, n}].transcript + "D"
+	delete_distance := edit_map[edit_key{m - 1, n}].distance - 8
+
+	if edit_map[edit_key{m, n - 1}].transcript == "" {
+		insert_map := EditDistanceBLOSUM(str1, str2[0:n-1], edit_map, BLOSOM_matrix)
+		maps.Copy(edit_map, insert_map)
+	}
+	insert_transcript := edit_map[edit_key{m, n - 1}].transcript + "I"
+	insert_distance := edit_map[edit_key{m, n - 1}].distance - 8
+
+	if edit_map[edit_key{m - 1, n - 1}].transcript == "" {
+		substitute_map := EditDistanceBLOSUM(str1[0:m-1], str2[0:n-1], edit_map, BLOSOM_matrix)
+		maps.Copy(edit_map, substitute_map)
+	}
+	a := str1[m-1:]
+	b := str2[n-1:]
+	substitute_transcript := edit_map[edit_key{m - 1, n - 1}].transcript
+	substitute_distance := edit_map[edit_key{m - 1, n - 1}].distance + BLOSOM_matrix[a][b]
+	if a == b {
+		substitute_transcript += "M"
+	} else {
+		substitute_transcript += "R"
+	}
+
+	distance := max(delete_distance, insert_distance, substitute_distance)
+
+	switch distance {
+	case delete_distance:
+		edit_map[edit_key{m, n}] = edit_info{delete_transcript, distance}
+		return edit_map
+	case insert_distance:
+		edit_map[edit_key{m, n}] = edit_info{insert_transcript, distance}
+		return edit_map
+	case substitute_distance:
+		edit_map[edit_key{m, n}] = edit_info{substitute_transcript, distance}
+		return edit_map
+	default:
+		return edit_map
+	}
+}
